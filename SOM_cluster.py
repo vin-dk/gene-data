571ee35f56145245c373c2f403895fd804774611
vin-dk <105610962+vin-dk@users.noreply.github.com>
2023-11-15 03:20:31 -0500

    SOM drastically improved

--- SOM_cluster.py
+++ SOM_cluster.py
@@ -1,14 +1,16 @@
 import numpy as np
-from minisom import MiniSom  
+from minisom import MiniSom 
 import matplotlib.pyplot as plt
 from Gene_Helper import GeneDataManager
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
-
+x = 5
+y = 5
 
 class SOMClusterer:
     @staticmethod
-    def perform_som_clustering(gene_manager, selected_features, grid_size=(10, 10), iterations=1000):
+    def perform_som_clustering(gene_manager, selected_features, grid_size=(x, y), iterations=1000):
         gene_data_list = gene_manager.fetch_gene_data()
 
         data = []
@@ -17,28 +19,28 @@ class SOMClusterer:
             data.append(feature_vector)
 
         data = np.array(data)
-        data = (data - data.min(axis=0)) / (data.max(axis=0) - data.min(axis=0))
+
+        # Use StandardScaler for feature scaling
+        scaler = StandardScaler()
+        data = scaler.fit_transform(data)
 
         som = MiniSom(grid_size[0], grid_size[1], data.shape[1], sigma=1.0, learning_rate=0.5)
         som.random_weights_init(data)
         som.train_batch(data, iterations, verbose=True)
 
-        plt.figure(figsize=(10, 10))
-        plt.pcolor(som.distance_map().T, cmap='bone_r')
-        plt.colorbar()
-
         winning_neurons = np.array([som.winner(data[i]) for i in range(len(data))])
 
-        cluster_data = {key: [] for key in [(i, j) for i in range(grid_size[0]) for j in range(grid_size[1])]}
+        # Extract the actual grid size used by MiniSom
+        actual_grid_size = som.get_weights().shape[:2]
 
-        for i, j in cluster_data.keys():
-            indices = np.where(np.logical_and(winning_neurons[:, 0] == i, winning_neurons[:, 1] == j))
+        # Calculate cluster assignments for each data point based on winning neurons
+        cluster_assignments = [i * actual_grid_size[1] + j for i, j in winning_neurons]
 
-            if len(indices[0]) > 0:
-                cluster_data[(i, j)] = indices[0]
+        # Display clusters
+        for cluster_idx in range(actual_grid_size[0] * actual_grid_size[1]):
+            cluster_name = f'Cluster {cluster_idx}'
+            indices = np.where(np.array(cluster_assignments) == cluster_idx)[0]
 
-        for (i, j), indices in cluster_data.items():
-            cluster_name = f'Cluster ({i}, {j})'
             print(f'\n{cluster_name}:')
 
             if len(indices) > 0:
@@ -52,7 +54,11 @@ class SOMClusterer:
 
                 print(f'Total Data Points in {cluster_name}: {len(indices)}')
 
+        plt.figure(figsize=(x, y))
+        plt.pcolor(som.distance_map().T[:actual_grid_size[0], :actual_grid_size[1]], cmap='bone_r')  # Crop the displayed part
+        plt.colorbar()
         plt.show()
+
 
 gene_manager = GeneDataManager('localhost', 'root', '720027924', 'pineal_data')
 selected_features = ['embryonic_day_21', 'postnatal_day_5', 'postnatal_day_20', 'postnatal_day_40',
